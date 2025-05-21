009b17912c480f591d5e8a89fb8d62c44615d4d5
qidi.chen <qidi.chen@q2bi.com>
2025-05-21 12:25:27 +0800

    feat: Add gemini-2.5-flash-preview-05-20

--- app/constant.ts
+++ app/constant.ts
@@ -548,6 +548,7 @@ const googleModels = [
   "gemini-2.0-pro-exp-02-05",
   "gemini-2.5-pro-preview-03-25",
   "gemini-2.5-flash-preview-04-17",
+  "gemini-2.5-flash-preview-05-20",
   "gemini-2.5-pro-preview-05-06",
 ];
 
